d6621ba7404371676c219d1919e062f93fc871b9
haqqi <haqqi@glovory.com>
2020-06-17 13:06:29 +0700

    🐨 [ADD] update pubspec

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:showcase/main.dart';
+import 'package:glova_showcase/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
