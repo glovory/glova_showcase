89d32c13e0f6919c617cf22a5e3eb172ab353b15
haqqi <haqqi@glovory.com>
2020-06-18 08:09:05 +0700

    🖨 [PRE] prepare button page as home. Need rework later.

---
+++ lib/component/button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ComponentButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buttons'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
