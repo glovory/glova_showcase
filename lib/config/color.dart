d0e6746a9550c61e907d658055413b28ce50a598
romaiswara <iswararoma@gmail.com>
2020-06-24 14:00:27 +0700

    generate new color

---
+++ lib/config/color.dart
@@ -0,0 +1,150 @@
+import 'dart:ui';
+
+import 'package:eva_color/eva_color.dart';
+
+// @autogenerate DO NOT EDIT!!!
+class AppColor {
+  static const EvaColor primary = EvaColor(0xFFF8B195, {
+    100: Color(0xFFFEF5EA),
+    200: Color(0xFFFEE9D5),
+    300: Color(0xFFFCD9C0),
+    400: Color(0xFFFACAAF),
+    500: Color(0xFFF8B195),
+    600: Color(0xFFD5836C),
+    700: Color(0xFFB25A4B),
+    800: Color(0xFF8F372F),
+    900: Color(0xFF771D1C),
+  });
+
+  static const EvaTransparentColor primaryTransparent =
+      EvaTransparentColor(0x66F8B195, {
+    100: Color(0x14F8B195),
+    200: Color(0x28F8B195),
+    300: Color(0x3DF8B195),
+    400: Color(0x51F8B195),
+    500: Color(0x66F8B195),
+    600: Color(0x7AF8B195),
+  });
+
+  static const EvaColor success = EvaColor(0xFFF67280, {
+    100: Color(0xFFFEEAE3),
+    200: Color(0xFFFED0C7),
+    300: Color(0xFFFCB1AB),
+    400: Color(0xFFF99596),
+    500: Color(0xFFF67280),
+    600: Color(0xFFD3536C),
+    700: Color(0xFFB1395C),
+    800: Color(0xFF8E244C),
+    900: Color(0xFF761543),
+  });
+
+  static const EvaTransparentColor successTransparent =
+      EvaTransparentColor(0x66F67280, {
+    100: Color(0x14F67280),
+    200: Color(0x28F67280),
+    300: Color(0x3DF67280),
+    400: Color(0x51F67280),
+    500: Color(0x66F67280),
+    600: Color(0x7AF67280),
+  });
+
+  static const EvaColor info = EvaColor(0xFFC06C84, {
+    100: Color(0xFFFBE8E5),
+    200: Color(0xFFF8CDCD),
+    300: Color(0xFFECAEB3),
+    400: Color(0xFFD9919F),
+    500: Color(0xFFC06C84),
+    600: Color(0xFFA54E6F),
+    700: Color(0xFF8A365E),
+    800: Color(0xFF6F224E),
+    900: Color(0xFF5C1444),
+  });
+
+  static const EvaTransparentColor infoTransparent =
+      EvaTransparentColor(0x66C06C84, {
+    100: Color(0x14C06C84),
+    200: Color(0x28C06C84),
+    300: Color(0x3DC06C84),
+    400: Color(0x51C06C84),
+    500: Color(0x66C06C84),
+    600: Color(0x7AC06C84),
+  });
+
+  static const EvaColor warning = EvaColor(0xFF6C5B7B, {
+    100: Color(0xFFF5EBF8),
+    200: Color(0xFFE9D8F1),
+    300: Color(0xFFCBB5D7),
+    400: Color(0xFFA18DAF),
+    500: Color(0xFF6C5B7B),
+    600: Color(0xFF554269),
+    700: Color(0xFF402D58),
+    800: Color(0xFF2C1D47),
+    900: Color(0xFF1E113B),
+  });
+
+  static const EvaTransparentColor warningTransparent =
+      EvaTransparentColor(0x666C5B7B, {
+    100: Color(0x146C5B7B),
+    200: Color(0x286C5B7B),
+    300: Color(0x3D6C5B7B),
+    400: Color(0x516C5B7B),
+    500: Color(0x666C5B7B),
+    600: Color(0x7A6C5B7B),
+  });
+
+  static const EvaColor danger = EvaColor(0xFF355C7D, {
+    100: Color(0xFFDBF1F8),
+    200: Color(0xFFBAE1F2),
+    300: Color(0xFF8DBDD8),
+    400: Color(0xFF6492B1),
+    500: Color(0xFF355C7D),
+    600: Color(0xFF26486B),
+    700: Color(0xFF1A3659),
+    800: Color(0xFF102648),
+    900: Color(0xFF0A1A3B),
+  });
+
+  static const EvaTransparentColor dangerTransparent =
+      EvaTransparentColor(0x66355C7D, {
+    100: Color(0x14355C7D),
+    200: Color(0x28355C7D),
+    300: Color(0x3D355C7D),
+    400: Color(0x51355C7D),
+    500: Color(0x66355C7D),
+    600: Color(0x7A355C7D),
+  });
+
+  static const EvaBasicColor basic = EvaBasicColor(0xFF8F9BB3, {
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFF7F9FC),
+    300: Color(0xFFEDF1F7),
+    400: Color(0xFFE4E9F2),
+    500: Color(0xFFC5CEE0),
+    600: Color(0xFF8F9BB3),
+    700: Color(0xFF2E3A59),
+    800: Color(0xFF222B45),
+    900: Color(0xFF192038),
+    1000: Color(0xFF151A30),
+    1100: Color(0xFF101426),
+  });
+
+  static const EvaTransparentColor basicLightTransparent =
+      EvaTransparentColor(0x668E9BB3, {
+    100: Color(0x148E9BB3),
+    200: Color(0x288E9BB3),
+    300: Color(0x3D8E9BB3),
+    400: Color(0x518E9BB3),
+    500: Color(0x668E9BB3),
+    600: Color(0x7A8E9BB3),
+  });
+
+  static const EvaTransparentColor basicDarkTransparent =
+      EvaTransparentColor(0x66FFFFFF, {
+    100: Color(0x14FFFFE1),
+    200: Color(0x28FFFFFF),
+    300: Color(0x3DFFFFFF),
+    400: Color(0x51FFFFFF),
+    500: Color(0x66FFFFFF),
+    600: Color(0x7AFFFFFF),
+  });
+}
